e1029c4b68b67b4313388a23a189d0099296f9c0
kategrytsenko <grytsenko.kate.ua@gmail.com>
2022-04-05 15:43:00 +0300

    OnPush strategy

--- src/app/components-types/components/user-card/user-card.component.ts
+++ src/app/components-types/components/user-card/user-card.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { User } from '../../interfaces';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserCardComponent {
   @Input() user!: User;
+
+  // executeFunction() {
+  //   console.log("App Rerendered");
+  //
+  //   return "This is Child Component"
+  // }
 }
