934d22d2e5f1e0683d35201096ffad9afba20d5b
Serhii Koshman <koshman.animys@gmail.com>
2022-05-01 18:21:13 +0300

    add UT for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
+  const elements = {
+    getHeader: () =>
+      fixture.debugElement.query(By.css('[data-role="app-header"]')),
+    getFooter: () =>
+      fixture.debugElement.query(By.css('[data-role="app-footer"]')),
+    getRouterLinks: () =>
+      fixture.debugElement.queryAll(By.css('[data-role="navigation-link"]')),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'practical-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('practical-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('practical-angular app is running!');
+  });
+
+  describe('common rendering', () => {
+    it('should render header', () => {
+      expect(elements.getHeader()).toBeTruthy();
+    });
+
+    it('should render footer', () => {
+      expect(elements.getFooter()).toBeTruthy();
+    });
+  });
+
+  it('should render correct amount of router links', () => {
+    const currentNumberOfRouterLinks = 4;
+
+    expect(elements.getRouterLinks().length).toBe(currentNumberOfRouterLinks);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  navigation = [
+  public navigation = [
     {
       path: '/rendering',
-      name: 'Rendering'
+      name: 'Rendering',
     },
     {
       path: '/template-rendering',
-      name: 'Template rendering'
+      name: 'Template rendering',
     },
     {
       path: '/components-types',
-      name: 'Components'
+      name: 'Components',
     },
     {
       path: '/resolver',
-      name: 'Resolver'
+      name: 'Resolver',
     },
   ];
 }
